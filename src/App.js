cd981c51a7228e673dc713e9f2eb014e2e1c99db
Indy Sowy <indira.sowy@gmail.com>
2023-01-13 21:09:52 -0800

    added files

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import styled from 'styled-components'
 import ScrollToTop from './hooks/ScrollToTop.js'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import Signup from './pages/Signup.jsx'
+import Community from './pages/Community.jsx'
+import Post from './pages/Post.jsx'
+import Upload from './pages/Upload.jsx'
+import SavedPosts from './pages/SavedPosts.jsx'
+
+export const DetailsContext = React.createContext(false);
+
 function App() {
   return (
-    <div className="App">
-      <h1>HI</h1>
-    </div>
-  //   <BrowserRouter>
-  //   <ScrollToTop />
-  //   <Navbar />
-  //         <Routes>
-  //           <Route path='/'>
-  //                 <Route index element={<Home />} />
-  //                 <Route path='about' element={<About />} />
-  //             </Route>
-  //         </Routes>
-  //   <Footer />
-  // </BrowserRouter>
+    // <div className="App">
+    //   <h1>HI</h1>
+    // </div>
+    <BrowserRouter>
+    <ScrollToTop />
+    {/* <Navbar /> */}
+          <Routes>
+            <Route path='/'>
+                  <Route index element={<Signup />} />
+                  <Route path='community' element={<Community />} />
+                  <Route path='post' element={<Post />} />
+                  <Route path='upload' element={<Upload />} />
+                  <Route path='saves' element={<SavedPosts />} />
+              </Route>
+          </Routes>
+    {/* <Footer /> */}
+  </BrowserRouter>
   );
 }
 
---
+++ src/pages/SavedPosts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SavedPosts = () => {
+    return (
+        <div>
+            <h1>Saved Possts</h1>
+        </div>
+    )
+}
+
+export default SavedPosts
